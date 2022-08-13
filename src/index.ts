caad167991e9214cd9aa0f01b82bc6f4ed0da2b6
Ramon Trepat <r.trepis@gmail.com>
2022-08-13 13:23:40 +0200

    Added function connect db

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,17 @@ import Debug from "debug";
 import chalk from "chalk";
 import express from "express";
 import morgan from "morgan";
+import mongoose from "mongoose";
 import startServer from "./server/startServer";
+import connectDB from "./database/connectDB";
 
 const app = express();
 const debug = Debug("robots:index");
 
 const port = process.env.PORT ?? 4000;
+const mongoURL = process.env.MONGO_URI;
 
 app.use(morgan("dev"));
 
 startServer(+port);
+connectDB(mongoURL);
