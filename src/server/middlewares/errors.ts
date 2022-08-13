4de4e8e19723ee40529959060f27ed21a1764685
David Lozano <dawgun@hotmail.com>
2022-08-13 19:17:38 +0200

    Created debug for read server message

--- src/server/middlewares/errors.ts
+++ src/server/middlewares/errors.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
+import Debug from "debug";
+import chalk from "chalk";
+
+const debug = Debug("robots:server:middlewares:errors");
 
 export const endpointError = (req: Request, res: Response) => {
   res.status(404).json({ error: "Endpoint not found" });
+
+  debug(chalk.red("Client sends an unknown endpoint"));
 };
 
 export default endpointError;
