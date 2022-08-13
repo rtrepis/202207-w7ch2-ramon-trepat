31460eef292618345c6a9e934b57c7945ee82b0b
Ramon Trepat <r.trepis@gmail.com>
2022-08-13 13:29:09 +0200

    Remove index unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import "./loadEnvironment";
-import Debug from "debug";
-import chalk from "chalk";
 import express from "express";
 import morgan from "morgan";
-import mongoose from "mongoose";
 import startServer from "./server/startServer";
 import connectDB from "./database/connectDB";
 
 const app = express();
-const debug = Debug("robots:index");
 
 const port = process.env.PORT ?? 4000;
 const mongoURL = process.env.MONGO_URI;
