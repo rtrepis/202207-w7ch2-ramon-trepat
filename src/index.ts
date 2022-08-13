a0087de1db7bf93a2f6af6401850eadaff3a2bbf
Ramon Trepat <r.trepis@gmail.com>
2022-08-13 11:33:01 +0200

    Added express morgan debug chalk debug dotenv listen

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import "./loadEnvironment";
+import Debug from "debug";
+import express from "express";
+import chalk from "chalk";
+import morgan from "morgan";
 
+const debug = Debug("robots:index");
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(morgan("dev"));
+
+app.listen(port, () => {
+  debug(chalk.blue(`Serve listening on http://localhost:${port}`));
+});
