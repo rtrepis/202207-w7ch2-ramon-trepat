ecce3daad8130cfe1c040ad1bc1b9736b0f6cb3f
Ramon Trepat <r.trepis@gmail.com>
2022-08-13 21:21:51 +0200

    Added function generalError test

--- src/server/middlewares/errors.test.ts
+++ src/server/middlewares/errors.test.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
-import { endpointError } from "./errors";
+import CustomError from "../types/serverTypes";
+import { endpointError, generalError } from "./errors";
 
 describe("Given the endpointError middleware", () => {
   describe("When it receives a response", () => {
-    test("Then it should call the response method status with 404", async () => {
-      const req: Partial<Request> = {};
-      const res: Partial<Response> = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
+    const req: Partial<Request> = {};
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    test("Then it should call the response method status with 404", () => {
       const status = 404;
 
       endpointError(req as Request, res as Response);
@@ -16,17 +17,36 @@ describe("Given the endpointError middleware", () => {
       expect(res.status).toHaveBeenCalledWith(status);
     });
 
-    test("Then it should call the response method json with an endpoint error", async () => {
-      const req: Partial<Request> = {};
-      const res: Partial<Response> = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
+    test("Then it should call the response method json with an endpoint error", () => {
       const error = { error: "Endpoint not found" };
 
       endpointError(req as Request, res as Response);
 
       expect(res.json).toHaveBeenCalledWith(error);
     });
+  });
+});
+
+describe("Give the generalErors", () => {
+  describe("Then it recives a custom error and response", () => {
+    test("Then it should call the response with this erro and response", () => {
+      const req: Partial<Request> = {};
+      const res: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const next = jest.fn();
+      const error = new Error() as CustomError;
+      error.code = 409;
+      error.privatMessage = "Privat Message";
+      error.publicMessage = "Public Message";
+
+      generalError(error, req as Request, res as Response, next);
+
+      expect(res.status).toHaveBeenCalledWith(error.code);
+      expect(res.json).toHaveBeenCalledWith({ error: error.publicMessage });
+    });
+
+    test("Then it should call the response method json with an endpoint error", () => {});
   });
 });
