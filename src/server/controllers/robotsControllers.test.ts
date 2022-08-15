66fb3902643ea0e7ac7295278df3464802eb1003
David Lozano <dawgun@hotmail.com>
2022-08-15 23:31:02 +0200

    Created test for deleteRobot controller

--- src/server/controllers/robotsControllers.test.ts
+++ src/server/controllers/robotsControllers.test.ts
@@ -1,24 +1,26 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import Robot from "../../database/models/Robot";
-import { getRobots } from "./robotsControllers";
+import createCustomError from "../../utils/errors";
+import { deleteRobot, getRobots } from "./robotsControllers";
 
-describe("Given the getRobots controller", () => {
-  describe("When it recieves a response", () => {
-    test("Then it should call the response method status with 200", async () => {
-      const req: Partial<Request> = {};
-      const res: Partial<Response> = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
-      const status = 200;
+describe("Given a controller", () => {
+  describe("When getRobots it's called", () => {
+    describe("And it receives a response", () => {
+      test("Then it should call the response method status with 200", async () => {
+        const req: Partial<Request> = {};
+        const res: Partial<Response> = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn(),
+        };
+        const status = 200;
 
-      Robot.find = jest.fn();
+        Robot.find = jest.fn();
 
-      await getRobots(req as Request, res as Response);
+        await getRobots(req as Request, res as Response);
 
-      expect(res.status).toHaveBeenCalledWith(status);
+        expect(res.status).toHaveBeenCalledWith(status);
+      });
     });
-
     describe("And Robot.find return a list with Mazinger and Optimus", () => {
       test("Then it should call the response method json with Mazinger and Optimus", async () => {
         const req: Partial<Request> = {};
@@ -40,6 +42,76 @@ describe("Given the getRobots controller", () => {
         await getRobots(req as Request, res as Response);
 
         expect(res.json).toHaveBeenCalledWith({ robots: robotList });
+      });
+    });
+  });
+
+  describe("When deleteRobot it's called", () => {
+    describe("And it receives a response with correct id", () => {
+      const req: Partial<Request> = { params: { idRobot: "1" } };
+      const res: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const next = jest.fn() as Partial<NextFunction>;
+
+      test("Then it should call the response method status with 202", async () => {
+        const status = 202;
+
+        Robot.findById = jest.fn();
+        Robot.deleteOne = jest.fn();
+
+        await deleteRobot(
+          req as Request,
+          res as Response,
+          next as NextFunction
+        );
+
+        expect(res.status).toHaveBeenCalledWith(status);
+      });
+
+      test("Then it should call the response method json with message", async () => {
+        const messageJson = { message: "Robot has been deleted" };
+
+        Robot.findById = jest.fn();
+        Robot.deleteOne = jest.fn();
+
+        await deleteRobot(
+          req as Request,
+          res as Response,
+          next as NextFunction
+        );
+
+        expect(res.json).toHaveBeenCalledWith(messageJson);
+      });
+    });
+
+    describe("And it receives a response with wrong id", () => {
+      test("Then it should call next function with an error ", async () => {
+        const req: Partial<Request> = { params: { idRobot: "23" } };
+        const res: Partial<Response> = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn(),
+        };
+        const next = jest.fn() as Partial<NextFunction>;
+        const { idRobot } = req.params;
+
+        const customError = createCustomError(
+          404,
+          "Robot id doesn't exist",
+          `User introduced ${idRobot} and doesn't exists`
+        );
+
+        Robot.findById = jest.fn().mockRejectedValue(new Error());
+        Robot.deleteOne = jest.fn();
+
+        await deleteRobot(
+          req as Request,
+          res as Response,
+          next as NextFunction
+        );
+
+        expect(next).toHaveBeenCalledWith(customError);
       });
     });
   });
